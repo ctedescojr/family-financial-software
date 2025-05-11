f65ad60a135e727fba40356170c5fa80e21ae4fe
ctedescojr <ctedesco@usp.br>
2025-05-11 16:32:30 -0300

    expenses app created

---
+++ expenses/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create a "Local" class for places, with lat, long, city
+# First local has to be home, creating home with signas file
+
+# Create a vehicle class, with fuel for km/l, average cost by km
+
+# Create a product 
